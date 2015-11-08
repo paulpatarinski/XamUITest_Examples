0f76ccab5bd90d301e7e3b239afdc9e1e8268b0a
Paul Patarinski <paulpatarinski@gmail.com>
2015-11-08 10:28:13 -0600

    Adding an Objective C example

---
+++ src_ObjectiveC/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Paul Patarinski on 11/8/15.
+//  Copyright (c) 2015 Paul Patarinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src_ObjectiveC/HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Paul Patarinski on 11/8/15.
+//  Copyright (c) 2015 Paul Patarinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
