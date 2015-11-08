ff32db6b3b5bf1109b6cb594429a7d93604148d8
Paul Patarinski <paulpatarinski@gmail.com>
2015-11-08 16:25:54 -0600

    Adding login page UI

--- src_Cordova/www/js/index.js
+++ src_Cordova/www/js/index.js
@@ -33,18 +33,11 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+      var loginButton =  document.getElementById("loginButton");
+      loginButton.addEventListener("click", app.login);
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    login : function(){
+        alert("Login button clicked");
     }
 };
 
