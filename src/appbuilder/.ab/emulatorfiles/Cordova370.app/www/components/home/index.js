ccc9b948d760c69749a8db7f6ba1cf308333136a
Paul Patarinski <paulpatarinski@gmail.com>
2015-12-18 22:41:02 -0600

    Adding a sample Appbuilder app

---
+++ src/appbuilder/.ab/emulatorfiles/Cordova370.app/www/components/home/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+app.home = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_home
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_home
