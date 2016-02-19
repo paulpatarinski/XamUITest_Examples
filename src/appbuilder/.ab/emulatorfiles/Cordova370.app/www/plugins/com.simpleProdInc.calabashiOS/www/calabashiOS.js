2abcc438a37ecc197fd6dbfb22aef50fd96a10d5
Paul P <ppatarinski@paylocity.com>
2016-02-19 08:23:36 -0600

    Removin some files from source & updating the git ignore

--- src/appbuilder/.ab/emulatorfiles/Cordova370.app/www/plugins/com.simpleProdInc.calabashiOS/www/calabashiOS.js
+++
@@ -1,9 +0,0 @@
-cordova.define("com.simpleProdInc.calabashiOS.calabashiOS", function(require, exports, module) { /*global cordova, module*/
-
-module.exports = {
-	 start: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "CalabashiOSServer", "start", []);
-    }
-};
-
-});
